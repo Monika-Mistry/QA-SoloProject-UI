17229028e0207d880981cdce4cac4482a10c5f46
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-14 21:35:02 +0100

    displayresults.js removed code to add to netflix.html

--- scripts/NetflixDisplayResults.js
+++ scripts/NetflixDisplayResults.js
@@ -1,7 +1,5 @@
 let netflix = [];
 
-const getNetflixRecord = (id) => netflix.filter(value => value.netflixId === id).reduce(acc => acc);
-
 const displayResults = results => {
     netflix = [];
     let resultObj = JSON.parse(results);
@@ -13,24 +11,9 @@ const displayResults = results => {
         netflix.push(resultsObj);
     }
 
-    //keys for table headings
-    let keys = Object.keys(netflix[0]);
 
     //create table
     let resultTable = document.getElementById('resultTable');
-    resultTable.className = "table table-hover";
-    resultTable.id = "resultTable";
-
-    //create table header
-    let tableHead = resultTable.createTHead();
-    tableHead.className = "thead-light"
-
-    //headers for results table
-    keys.forEach(value => {
-        let cell = document.createElement('th');
-        cell.innerHTML = value;
-        tableHead.appendChild(cell);
-    })
 
     //create table body
     let tableBody = resultTable.createTBody();
