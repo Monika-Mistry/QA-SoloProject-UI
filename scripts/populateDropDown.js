5dd2198d69e54efa35c4092363ef85adbf026a27
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-17 17:19:49 +0100

    adding JS functionality to populate dropdown

---
+++ scripts/populateDropDown.js
@@ -0,0 +1,33 @@
+let results = [];
+const getTitles = () => {
+    makeRequest(
+        "GET",
+        "http://localhost:8080/netflixWatchlistApp/api/netflix/getAllProgrammes"
+    )
+        .then(response => {
+            populateDropDown(response)
+        })
+        .catch(error => console.log(error.message));
+}
+
+const populateTitles = (programmes) => {
+    results = [];
+    let resultObj = JSON.parse(results);
+    
+    //array of netflix programmes
+    if (Array.isArray(resultObj)) {
+        netflix = netflix.concat(resultObj);
+    } else {
+        netflix.push(resultObj);
+    }
+
+    //select dropdown
+    let dropdown = document.getElementById("netflixProgram");
+
+    //populate dropdown
+    results.forEach(value => {
+        
+    })
+
+
+};
