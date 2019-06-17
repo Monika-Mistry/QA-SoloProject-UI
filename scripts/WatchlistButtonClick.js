e73ec32bba6a6ebc3660b142494b7e1f6672b6f5
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-17 12:07:13 +0100

    method headers added

---
+++ scripts/WatchlistButtonClick.js
@@ -0,0 +1,8 @@
+//watchlist/addAProgram
+const addToWatchlist =() => {};
+//watchlist/removeAProgram/{id}
+const removeFromWatchlist =() => {};
+//watchlist/getWatchlist
+const getWatchlist =() => {};
+//watchlist/updateAProgram/{id}
+const updateWatchlistProgram =() => {};
