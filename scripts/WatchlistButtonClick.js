b2f468d4bd038445e9a197eb269b46cfdc69be60
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-17 12:14:59 +0100

    methods updated

--- scripts/WatchlistButtonClick.js
+++ scripts/WatchlistButtonClick.js
@@ -1,8 +1,50 @@
-//watchlist/addAProgram
-const addToWatchlist =() => {};
-//watchlist/removeAProgram/{id}
-const removeFromWatchlist =() => {};
-//watchlist/getWatchlist
-const getWatchlist =() => {};
+
+const addToWatchlist = () => {
+    const program;
+    makeRequest(
+        "POST",
+        "http://localhost:8080/netflixWatchlistApp/api/watchlist/addAProgram",
+        JSON.stringify(program)
+    )
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => console.log(error.message));
+};
+
+const removeFromWatchlist = () => {
+    let id;
+    makeRequest(
+        "DELETE",
+        `http://localhost:8080/netflixWatchlistApp/api/watchlist/removeAProgram\{id}`
+    )
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => console.log(error.message));
+};
+
+const getWatchlist = () => {
+    makeRequest(
+        "GET",
+        "http://localhost:8080/netflixWatchlistApp/api/watchlist/getWatchlist"
+    )
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => console.log(error.message));
+};
 //watchlist/updateAProgram/{id}
-const updateWatchlistProgram =() => {};
+const updateWatchlistProgram = () => {
+    const status;
+    let id;
+    makeRequest(
+        "PUT",
+        `http://localhost:8080/netflixWatchlistApp/api/watchlist/updateAProgram/{id}`,
+        JSON.stringify(status)
+    )
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => console.log(error.message));
+};
